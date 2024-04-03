3ccb66b7fd3e170e394c98654f605343b1c41ec6
otavio xavier <otavioxaviermarinho@gmail.com>
2024-04-02 22:34:19 -0300

    aula-2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Component } from "react";
+import { PostCard } from './components/PostCard';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    posts: [],
+  };
+
+  componentDidMount() {
+    this.loadPosts();
+  }
+
+  loadPosts = async () => {
+    const postsResponse = fetch("https://jsonplaceholder.typicode.com/posts"); //Requisição de posts
+    const photosResponse = fetch("https://jsonplaceholder.typicode.com/photos"); //Requisição de photos
+
+    const [posts, photos] = await Promise.all([postsResponse, photosResponse]); //response de posts e photos
+
+    const postsJson = await posts.json(); //tratamento de posts
+    const photosJson = await photos.json(); //tratamento de photos
+
+    const postsAndPhotos = postsJson.map((post, index) => {
+      return { ...post, cover: photosJson[index].url };
+    });
+
+    this.setState({ posts: postsAndPhotos }); //agregando o valor de posts ao estado
+  };
+
+  render() {
+    const { posts } = this.state;
+    return (
+      <section className="container">
+        <div className="posts">
+          {posts.map((post) => (
+           
+           <PostCard 
+            title={post.title}
+            body={post.body}
+            id={post.id}
+            cover={post.cover}
+            key={post.id}/>
+          ))}
+        </div>
+      </section>
+    );
+  }
 }
 
 export default App;
