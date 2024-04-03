55845905f9a4e0948e16a1367e725c1a9b0ad251
otavio xavier <otavioxaviermarinho@gmail.com>
2024-04-03 18:19:07 -0300

    aula-23

--- src/templates/home/index.jsx
+++ src/templates/home/index.jsx
@@ -2,12 +2,16 @@ import "./styles.css";
 
 import { Component } from "react";
 
-import { loadPosts } from "../../utils/load-posts"
-import { Posts } from '../../components/Posts';
+import { loadPosts } from "../../utils/load-posts";
+import { Posts } from "../../components/Posts";
+import { Button } from "../../components/Button";
 
 export class Home extends Component {
   state = {
     posts: [],
+    allPosts: [],
+    page: 0,
+    postsPerPage: 4,
   };
 
   async componentDidMount() {
@@ -15,16 +19,40 @@ export class Home extends Component {
   }
 
   loadPosts = async () => {
+    const { page, postsPerPage } = this.state;
     const postsAndPhotos = await loadPosts();
-    this.setState({posts: postsAndPhotos})
-  } 
+    this.setState({
+      posts: postsAndPhotos.slice(page, postsPerPage),
+      allPosts: postsAndPhotos,
+    });
+  };
+
+  loadMorePosts = () => {
+    const { page, postsPerPage, allPosts, posts } = this.state;
+    const nextPage = page + postsPerPage;
+    const newPosts = allPosts.slice(nextPage, nextPage + postsPerPage);
+
+    posts.push(...newPosts);
+
+    this.setState({ posts, page: nextPage });
+
+    console.log(page, postsPerPage, nextPage, nextPage + postsPerPage);
+  };
 
   render() {
-    const { posts } = this.state;
+    const { posts, page, postsPerPage, allPosts } = this.state;
+    const noMorePosts = page + postsPerPage >= allPosts.length;
 
     return (
       <section className="container">
-       <Posts posts={posts}/>
+        <Posts posts={posts} />
+        <div className='button-container'>
+          <Button
+            text={"load More Posts"}
+            onClick={this.loadMorePosts}
+            disabled={noMorePosts}
+          ></Button>
+        </div>
       </section>
     );
   }
